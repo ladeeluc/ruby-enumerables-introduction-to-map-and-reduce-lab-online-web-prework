8bf49bec60de5af25be279bd2bcaa356bc2fd95e
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-27 19:38:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,3 +58,13 @@ while counter < array.size do
 end
 total
 end
+
+def reduce_to_total(array,starting_point = 0)
+  total = starting_point
+  counter = 0
+while counter < array.size do
+    total += array[counter]
+    counter += 1
+end
+total
+end
